1919f65445b99a0d3a0b02bbbb27189f3913444e
Aryan Sisodiya <aryansisodiya091107@gmail.com>
2021-08-19 21:13:20 +0530

    lazy loading in react with React.lazy and Suspense + theme color and meta description added!

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,3 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import NavBar from "./components/NavBar";
-import Signup from "./components/Signup";
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,7 +6,20 @@ import {
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import LoadingBar from 'react-top-loading-bar';
-import React from "react";
+import React, { Suspense } from "react";
+import Spinner from "./components/Spinner";
+// import About from "./components/About";
+const About = React.lazy(() => import("./components/About"))
+// import Contact from "./components/Contact";
+const Contact = React.lazy(() => import("./components/Contact"))
+// import Home from "./components/Home";
+const Home = React.lazy(() => import("./components/Home"))
+// import Login from "./components/Login";
+const Login = React.lazy(() => import("./components/Login"))
+// import NavBar from "./components/NavBar";
+const NavBar = React.lazy(() => import("./components/NavBar"))
+// import Signup from "./components/Signup";
+const Signup = React.lazy(() => import("./components/Signup"))
 
 function App() {
   const [progress, setProgress] = React.useState(0);
@@ -28,24 +35,26 @@ function App() {
         onLoaderFinished={() => setProgress(0)}
         height={2.5}
       />
-      <NavBar loader={loader} />
-      <Switch>
-        <Route exact path="/">
-          <Home loader={loader} />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/contact">
-          <Contact loader={loader} />
-        </Route>
-        <Route exact path="/signin">
-          <Login loader={loader} />
-        </Route>
-        <Route exact path="/register">
-          <Signup loader={loader} />
-        </Route>
-      </Switch>
+      <Suspense fallback={<Spinner />}>
+        <NavBar loader={loader} />
+        <Switch>
+          <Route exact path="/">
+            <Home loader={loader} />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/contact">
+            <Contact loader={loader} />
+          </Route>
+          <Route exact path="/signin">
+            <Login loader={loader} />
+          </Route>
+          <Route exact path="/register">
+            <Signup loader={loader} />
+          </Route>
+        </Switch>
+      </Suspense>
 
     </Router>
   );
---
+++ client/src/components/Spinner.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Spinner = () => {
+    const style = {
+        width: "36vh",
+        height: "36vh"
+    }
+
+    const styleDiv = {
+        height: "100vh",
+        width: "100vw"
+    }
+
+    return (
+        <div className="d-flex justify-content-center align-items-center" style={styleDiv}>
+            <div class="spinner-border text-secondary" style={style} role="status">
+            </div>
+        </div>
+    )
+}
+
+export default Spinner
